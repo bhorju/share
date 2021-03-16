7fcd13eef9ad1351a2df325d99d6186fa268ebdc
bhorju <33804995+bhorju@users.noreply.github.com>
2021-03-16 18:08:23 +0700

    Update miner-microbitcoin.sh

--- miner-microbitcoin.sh
+++ miner-microbitcoin.sh
@@ -4,7 +4,7 @@
 #	stratum-ru.rplant.xyz   /Moscow/
 #	stratum-eu.rplant.xyz   /London/
 #	stratum-asia.rplant.xyz /Singapore/
-# stratum-na.rplant.xyz   /Toronto/
+#       stratum-na.rplant.xyz   /Toronto/
 #
 while [ 1 ]; do
 ./cpuminer-sse2amd -a power2b -o stratum+tcps://stratum-na.rplant.xyz:17022 -u MYoHx6jYqa5ZEBTXdKyUp4hYeg82dHSjd2.kewel
